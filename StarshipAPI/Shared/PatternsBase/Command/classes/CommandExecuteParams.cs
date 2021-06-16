bd2c4df3aad0d51d5be94baa8295b4209098bf22
Pholosho Motala <pmotala@student.wethinkcode.co.za>
2021-06-16 12:45:28 +0200

    Create Shared Resources
    Create Command base Interfaces and Classes

---
+++ StarshipAPI/Shared/PatternsBase/Command/classes/CommandExecuteParams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.PatternsBase.Command.classes
+{
+    public abstract class CommandExecuteParams
+    {
+        string _commandName;
+
+        public string CommandName { get { return this._commandName; } }
+
+        CommandExecuteParams(string commandName)
+        {
+            this._commandName = commandName;
+        }
+    }
+}
---
+++ StarshipAPI/Shared/PatternsBase/Command/interfaces/ICommandFactory.cs
@@ -0,0 +1,16 @@
+﻿using Command.classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.PatternsBase.Command.interfaces
+{
+    public interface ICommandFactory
+    {
+        String CommandName { get; }
+        String CommandDescription { get; }]
+
+        ICommand MakeCommand(CommandExecuteParams arguments);
+
+    }
+}
