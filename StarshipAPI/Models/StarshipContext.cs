9437804ec54c76e0df249e48297ed9232e1ff93e
Luke van Aardt <vanaardtluke@gmail.com>
2021-06-16 12:27:13 +0200

    base

---
+++ StarshipAPI/Models/StarshipContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace StarshipAPI.Models
+{
+    public class StarshipContext : DbContext
+    {
+        public StarshipContext(DbContextOptions<StarshipContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Crewmate> TodoItems { get; set; }
+    }
+}
